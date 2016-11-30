bf6d0733a0448a3abdea45d5421a751403b6dd5f
Alexey <korolev2009@gmail.com>
2016-11-30 18:43:14 +0200

    Instrumented tests - stage 3

--- app/src/androidTest/java/net/alexandroid/shprefhelper/LibraryMethodsTest.java
+++ app/src/androidTest/java/net/alexandroid/shprefhelper/LibraryMethodsTest.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -21,17 +22,19 @@ public class LibraryMethodsTest {
     private static final String VALUE_STRING = "Some string";
     private static final String DEFAULT_VALUE_STRING = "Some default value";
     private static final int VALUE_INTEGER = Integer.MAX_VALUE;
-    private static final boolean VALUE_BOOLEAN = true;
+    private static final int DEFAULT_VALUE_INTEGER = 777;
+    private static final boolean VALUE_BOOLEAN = false;
+    private static final boolean DEFAULT_VALUE_BOOLEAN = true;
     private static final float VALUE_FLOAT = Float.MAX_VALUE;
     private static final double VALUE_DOUBLE = Double.MAX_VALUE;
     private static final long VALUE_LONG = Long.MAX_VALUE;
-    private static final String VALUE_STRING_NULL = null;
 
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
         assertEquals("net.alexandroid.shprefhelper", appContext.getPackageName());
+        ShPref.init(appContext, ShPref.COMMIT);
     }
 
     @Before
@@ -40,7 +43,16 @@ public class LibraryMethodsTest {
         System.out.println("=== ShPref is cleared ===");
     }
 
-    // TODO Test clear method
+    // Clear
+
+    @Test
+    public void testClear() {
+        ShPref.put(KEY_1, VALUE_STRING);
+        ShPref.clear();
+        assertNull("Clear method doesn't remove all values from Shared Preferences", ShPref.getString(KEY_1));
+    }
+
+    // Contains
 
     @Test
     public void testContains() {
@@ -51,6 +63,16 @@ public class LibraryMethodsTest {
         ShPref.put(R.string.test_key_1, VALUE_STRING);
         isContains = ShPref.contains(R.string.test_key_1);
         assertEquals("Contains method return wrong result. 002", true, isContains);
+    }
+
+    // Put and get String
+    @Test
+    public void testPutStringNull() {
+        ShPref.put(KEY_1, null);
+        assertNull("String is not null as should be 001", ShPref.getString(KEY_1));
+
+        ShPref.put(R.string.test_key_1, null);
+        assertNull("String is not null as should be 002", ShPref.getString(R.string.test_key_1));
     }
 
     @Test
@@ -64,14 +86,69 @@ public class LibraryMethodsTest {
 
     @Test
     public void testGetStringDefaultValue() {
-        System.out.println("DEFAULT_VALUE_STRING: " + DEFAULT_VALUE_STRING);
-        System.out.println("Default value returned: " + ShPref.getString(KEY_1, DEFAULT_VALUE_STRING));
-        assertEquals("Method getString doesn't return the write default value 001",
+        assertEquals("Method getString doesn't return the default value 001",
                 DEFAULT_VALUE_STRING, ShPref.getString(KEY_1, DEFAULT_VALUE_STRING));
 
-        assertEquals("Method getString doesn't return the write default value 002",
+        assertEquals("Method getString doesn't return the default value 002",
                 DEFAULT_VALUE_STRING, ShPref.getString(R.string.test_key_1, DEFAULT_VALUE_STRING));
     }
+
+    // Put and get int
+
+    @Test
+    public void testPutAndGetInt() {
+        ShPref.put(KEY_1, VALUE_INTEGER);
+        assertEquals("Int put and get error 001", VALUE_INTEGER, ShPref.getInt(KEY_1));
+
+        ShPref.put(R.string.test_key_1, VALUE_INTEGER);
+        assertEquals("Int put and get error 002", VALUE_INTEGER, ShPref.getInt(R.string.test_key_1));
+    }
+
+    @Test
+    public void testGetIntDefaultValue() {
+        assertEquals("Method getInt doesn't return the default value 001",
+                DEFAULT_VALUE_INTEGER, ShPref.getInt(KEY_1, DEFAULT_VALUE_INTEGER));
+
+        assertEquals("Method getInt doesn't return the default value 002",
+                DEFAULT_VALUE_INTEGER, ShPref.getInt(R.string.test_key_1, DEFAULT_VALUE_INTEGER));
+    }
+
+
+    // Put and get boolean
+
+    @Test
+    public void testPutAndGetBoolean() {
+        ShPref.put(KEY_1, VALUE_BOOLEAN);
+        assertEquals("String put and get error 001", VALUE_BOOLEAN, ShPref.getBoolean(KEY_1));
+
+        ShPref.put(R.string.test_key_1, VALUE_BOOLEAN);
+        assertEquals("String put and get error 002", VALUE_BOOLEAN, ShPref.getBoolean(R.string.test_key_1));
+    }
+
+    @Test
+    public void testGetBooleanDefaultValue() {
+        assertEquals("Method getBoolean doesn't return the default value 001",
+                DEFAULT_VALUE_BOOLEAN, ShPref.getBoolean(KEY_1, DEFAULT_VALUE_BOOLEAN));
+
+        assertEquals("Method getBoolean doesn't return the default value 002",
+                DEFAULT_VALUE_BOOLEAN, ShPref.getBoolean(R.string.test_key_1, DEFAULT_VALUE_BOOLEAN));
+    }
+
+
+
+
+    // TODO float
+    // TODO double
+    // TODO long
+    // TODO putC
+    // TODO putA
+    // TODO putList
+    // TODO getMixed list
+    // TODO get
+    // TODO remove
+    // TODO removeA
+    // TODO removeC
+    // TODO Editor
 
 
 }
