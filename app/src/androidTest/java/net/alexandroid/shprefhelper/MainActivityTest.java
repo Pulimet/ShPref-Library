bca967eaef7f1a4dc595ca0cc980cd90174605c6
Alexey Korolev <korolev2009@gmail.com>
2016-12-04 16:45:17 +0200

    UI tests stage 2

--- app/src/androidTest/java/net/alexandroid/shprefhelper/MainActivityTest.java
+++ app/src/androidTest/java/net/alexandroid/shprefhelper/MainActivityTest.java
@@ -12,7 +12,6 @@ import android.widget.TextView;
 import net.alexandroid.shpref.ShPref;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,7 +24,7 @@ import static org.junit.Assert.assertTrue;
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
 
-    private  UiDevice mDevice;
+    private UiDevice mDevice;
 
     @Rule
     public MainActivityTestRule<MainActivity> mMainActivityActivityTestRule = new MainActivityTestRule<>(MainActivity.class);
@@ -38,65 +37,80 @@ public class MainActivityTest {
     }
 
     @Test
-    @Ignore
-    public void testUI() {
+    public void testUI() throws InterruptedException, UiObjectNotFoundException {
         Activity activity = mMainActivityActivityTestRule.getActivity();
         assertNotNull(activity.findViewById(R.id.textView));
         TextView textView = (TextView) activity.findViewById(R.id.textView);
         assertTrue(textView.isShown());
         assertEquals(textView.getText(), activity.getString(R.string.sample_app));
+
+        UiObject tv = getUiObjectByText(R.string.sample_app);
+        tv.setText("========== TEST ==========");
+        waitSeconds(2);
     }
 
     @Test
-    @Ignore
-    public void testPressBackButton() throws RemoteException {
+    public void testPressBackButton() throws RemoteException, InterruptedException {
         UiDevice mDevice = UiDevice.getInstance(getInstrumentation());
         if (mDevice.isScreenOn()) {
+            waitSeconds(2);
             mDevice.setOrientationLeft();
+            waitSeconds(2);
             mDevice.openNotification();
+            waitSeconds(2);
+            mDevice.setOrientationNatural();
+            waitSeconds(2);
         }
         mDevice.pressBack();
     }
 
-
-
     @Test
-    public void testSaveAndLoad() {
+    public void testSaveAndLoad() throws UiObjectNotFoundException, InterruptedException {
         UiObject loadBtn = getUiObjectByText(R.string.load);
         UiObject saveBtn = getUiObjectByText(R.string.save);
-        waitSeconds(1);
-        clickOnWidget(loadBtn);
-        clickOnWidget(saveBtn);
-        clickOnWidget(loadBtn);
+        clickOnWidgetAndWait(loadBtn);
+        clickOnWidgetAndWait(saveBtn);
+        clickOnWidgetAndWait(loadBtn);
     }
 
 
     @Test
-    public void testContainsRemove() {
+    public void testContainsRemove() throws UiObjectNotFoundException, InterruptedException {
         UiObject saveBtn = getUiObjectByText(R.string.save);
         UiObject containsBtn = getUiObjectByText(R.string.contains);
         UiObject removeBtn = getUiObjectByText(R.string.remove);
-        waitSeconds(1);
-        clickOnWidget(containsBtn);
-        clickOnWidget(saveBtn);
-        clickOnWidget(containsBtn);
-        clickOnWidget(removeBtn);
-        clickOnWidget(containsBtn);
+
+        clickOnWidgetAndWait(containsBtn);
+        clickOnWidgetAndWait(saveBtn);
+        clickOnWidgetAndWait(containsBtn);
+        clickOnWidgetAndWait(removeBtn);
+        clickOnWidgetAndWait(containsBtn);
     }
 
-    /*
 
     @Test
-    public void testSaveAndLoadWithEditor() {
+    public void testSaveAndLoadWithEditor() throws UiObjectNotFoundException, InterruptedException {
+        UiObject saveBtn = getUiObjectByText(R.string.save_with_editor);
+        UiObject loadBtn = getUiObjectByText(R.string.load2);
+        clickOnWidgetAndWait(loadBtn);
+        clickOnWidgetAndWait(saveBtn);
+        clickOnWidgetAndWait(loadBtn);
     }
 
     @Test
-    public void testLogger() {
+    public void testLogger() throws InterruptedException, UiObjectNotFoundException {
+        UiObject testLoggerBtn = getUiObjectByText(R.string.test_logger);
+        clickOnWidgetAndWait(testLoggerBtn);
     }
 
     @Test
-    public void testSaveAndLoadList() {
-    }*/
+    public void testSaveAndLoadList() throws InterruptedException, UiObjectNotFoundException {
+        UiObject saveBtn = getUiObjectByText(R.string.save_list);
+        UiObject loadBtn = getUiObjectByText(R.string.load_list);
+        clickOnWidgetAndWait(loadBtn);
+        clickOnWidgetAndWait(saveBtn);
+        clickOnWidgetAndWait(loadBtn);
+    }
 
     private UiObject getUiObjectByText(int stringRes) {
         Activity activity = mMainActivityActivityTestRule.getActivity();
@@ -104,21 +118,13 @@ public class MainActivityTest {
         return mDevice.findObject(selector);
     }
 
-    private void clickOnWidget(UiObject aWidget) {
-        try {
-            aWidget.click();
-        } catch (UiObjectNotFoundException e) {
-            e.printStackTrace();
-        }
+    private void clickOnWidgetAndWait(UiObject aWidget) throws UiObjectNotFoundException, InterruptedException {
+        aWidget.click();
         waitSeconds(4);
     }
 
-    private void waitSeconds(int sec) {
-        try {
-            Thread.sleep(sec * 1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    private void waitSeconds(int sec) throws InterruptedException {
+        Thread.sleep(sec * 1000);
     }
 
 
